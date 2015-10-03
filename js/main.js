c01a5912e244e337abd99391d439278c7d549ab2
joel swensen <joelmswensen@gmail.com>
2015-10-03 14:50:42 -0700

    Added all project files and pictures

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    $('.main-header').hide().fadeIn(2000);
+    $('#fref1').hide().fadeIn(3000);
+    $('#fref2').hide().fadeIn(2500);
+    $('#fref3').hide().fadeIn(2000);
+    $('#fref4').hide().fadeIn(1500);
+    $('.three-words').hide().fadeIn(6000);
+
+});
